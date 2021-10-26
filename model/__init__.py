6558f975cbe3510268f908ff8a5f25912e3c8318
Yiujin <ujin2us@gmail.com>
2021-10-26 09:48:14 +0000

    feat: modify fixing random seed

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from .basic_model import *
 from .fcn import *
+from .unet import *
 
-__all__ = ["BasicModel", "BasicModel2", "FCN8s", "FCN16s", "FCN32s"]
+__all__ = ["BasicModel", "BasicModel2", "FCN8s", "FCN16s", "FCN32s", "UNet"]
