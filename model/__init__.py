e664c215a58185544f684e02573945213c16d9c7
swkim-sm <swit1712155@sookmyung.ac.kr>
2021-10-26 07:44:01 +0000

    feat: add HRNet

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,5 @@
 from .basic_model import *
 from .fcn import *
+from .hrnet import *
 
-__all__ = [
-    "BasicModel",
-    "BasicModel2",
-    "FCN8s", "FCN16s", "FCN32s"
-]
+__all__ = ["BasicModel", "BasicModel2", "FCN8s", "FCN16s", "FCN32s", "HRNet"]
