0404eb2483d419e7f9fd712d4c9e4fe42f76624f
JODONG2 <whgusehd96@gmail.com>
2021-10-26 07:11:15 +0000

    Add data augmentation

--- data_loader/transforms.py
+++ data_loader/transforms.py
@@ -2,12 +2,44 @@ from pycocotools.coco import COCO
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from data_loader.custom_transform import Elastic_Transform
+from albumentations import ElasticTransform
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,
+    CenterCrop,
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion,
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,
+    RandomGamma,
+    HueSaturationValue,
+    RGBShift,
+    RandomBrightness,
+    RandomContrast,
+    Resize,
+    MotionBlur,
+    MedianBlur,
+    GaussianBlur,
+    GaussNoise,
+    GridDropout,  # GridMask
+    ChannelShuffle,
+    CoarseDropout,  # Cutout
+)
+from albumentations.augmentations.crops.transforms import CropNonEmptyMaskIfExists
 
 
-def ElasticTransform():
+def Elastic_Transform():
     return A.Compose(
         [
-            Elastic_Transform(p=0.5),
+            ElasticTransform(),
             ToTensorV2(),
         ]
     )
@@ -16,6 +48,17 @@ def ElasticTransform():
 def BasicTransform():
     return A.Compose(
         [
+            ToTensorV2(),
+        ]
+    )
+
+
+def CustomTransform():
+    return A.Compose(
+        [
+            CropNonEmptyMaskIfExists(height=256, width=256, p=0.5),
+            Resize(512, 512),
+            GridDropout(ratio=0.2, holes_number_x=5, holes_number_y=5, random_offset=True, p=0.5),
             ToTensorV2(),
         ]
     )
