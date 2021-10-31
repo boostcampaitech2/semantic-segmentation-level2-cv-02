bb1a68b2c4456becbf98ce9be15a115b74b8b352
swkim-sm <swit1712155@sookmyung.ac.kr>
2021-10-31 18:29:00 +0000

    tmp

--- model/__init__.py
+++ model/__init__.py
@@ -2,5 +2,6 @@ from .basic_model import *
 from .fcn import *
 from .unet import *
 from .hrnet import *
+from .hrnet_ocr import *
 
-__all__ = ["BasicModel", "BasicModel2", "FCN8s", "FCN16s", "FCN32s", "UNet", "UnetPlusPlus", "HRNet"]
+__all__ = ["BasicModel", "BasicModel2", "FCN8s", "FCN16s", "FCN32s", "UNet", "UnetPlusPlus", "HRNet", "HRNet_OCR"]
